23408773c300e9327c8195d1d0dfbdf641206996
Ravikant Prasad <ravik.lkp@gmail.com>
2024-08-02 15:02:44 +0530

    nwe as raviportfolio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,10 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
